ba4ca9cb0b65a120e8b7eed8f51ebc06cdff7a5a
sodapop <65791614+sodapop1104@users.noreply.github.com>
2026-03-10 00:05:16 -0700

    updated script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// Subscribe feature (all pages)
 function subscribeUser(event) {
   event.preventDefault();
   alert("Thank you for subscribing.");
 }
 
-// Gallery page buttons
 function addToCart() {
   alert("Item added to the cart.");
 }
@@ -17,7 +15,6 @@ function processOrder() {
   alert("Thank you for your order.");
 }
 
-// About Us / Contact page
 function submitMessage(event) {
   event.preventDefault();
   alert("Thank you for your message.");
